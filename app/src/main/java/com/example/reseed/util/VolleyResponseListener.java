245ff02816756fe1a30e362eda5e75293436c6b2
david <lightspot@gmail.com>
2023-03-26 09:07:10 +0200

    Change name of the root. Added more errors checks for the login request. Eliminate the appactivity from the history when logout.

--- app/src/main/java/com/example/reseed/util/VolleyResponseListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.reseed.util;
-
-/**
- * Response listener for the LoginActivity class.
- */
-public interface VolleyResponseListener {
-    void onError(String message);
-    void onResponse(Object response);
-}
