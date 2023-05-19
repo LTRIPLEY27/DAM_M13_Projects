5d768df436adf0d39aed551fc6ec637a3ba7e05e
david <lightspot@gmail.com>
2023-05-19 07:47:44 +0200

    Creating logic for stadistics part 2.

--- app/src/main/java/com/reseed/fragments/FragmentAdminStadistics.java
+++ app/src/main/java/com/reseed/fragments/FragmentAdminStadistics.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -22,6 +23,7 @@ import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
 import com.reseed.R;
 import com.reseed.interfaces.VolleyResponseInterface;
+import com.reseed.requests.JsonArrayGetRequest;
 import com.reseed.requests.SingletonReqQueue;
 import com.reseed.requests.UserListRequest;
 
@@ -36,9 +38,11 @@ public class FragmentAdminStadistics extends Fragment {
 
     PieChart pieChart;
 
-    String userToken;
+    String userToken, typeUser, spinnerStatusData;
 
-    Spinner spinnerTecnicos;
+    Spinner spinnerEstatus;
+
+    Button filterButton;
 
     JSONArray jsonArrayUsers;
 
@@ -55,7 +59,8 @@ public class FragmentAdminStadistics extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-
+            this.userToken = getArguments().getString("token");
+            this.typeUser = getArguments().getString("typeUser");
         }
 
         // instanciamos la requestQueue
@@ -63,6 +68,7 @@ public class FragmentAdminStadistics extends Fragment {
 
         //creamos los objetos
         jsonArrayUsers = new JSONArray();
+        idsUsers = new ArrayList<>();
 
 
 
@@ -78,7 +84,9 @@ public class FragmentAdminStadistics extends Fragment {
         // Preparamos el arraylist que contendra los datos de las estadisticas.
         ArrayList<PieEntry> pieEntries = new ArrayList<>();
 
-        spinnerTecnicos = view.findViewById(R.id.spinnerUsuariosTecnicos);
+        spinnerEstatus = view.findViewById(R.id.spinnerEstado);
+        filterButton = view.findViewById(R.id.filtrarBtn);
+
 
         // obtenemos el objeto para realizar las estadisticas.
         pieChart = view.findViewById(R.id.estadisticaPastel);
@@ -116,7 +124,21 @@ public class FragmentAdminStadistics extends Fragment {
 
         pieChart.animateX(1000, Easing.EaseInOutCubic);
 
-        requestData(userToken);
+        //requestUserListData(userToken);
+
+        // Listener del boton de filtrar.
+        filterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(){
+                    
+                }
+
+            }
+        });
+
+
 
         return view;
     }
@@ -126,7 +148,7 @@ public class FragmentAdminStadistics extends Fragment {
      *
      * @param userToken
      */
-    private void requestData(String userToken) {
+    private void requestUserListData(String userToken) {
 
         UserListRequest userListRequest = new UserListRequest(userToken, "tecnicos", requestQueue);
         JSONObject jsResponse = new JSONObject();
@@ -151,7 +173,7 @@ public class FragmentAdminStadistics extends Fragment {
                 try {
                     arrayAdapter = new ArrayAdapter(
                             getContext(),
-                            android.R.layout.simple_spinner_item,
+                            android.R.layout.simple_spinner_dropdown_item,
                             arrayUsersName(jsResponse.getJSONArray(0),idsUsers));
                 } catch (JSONException e) {
                     Log.e("Error peticion users", e.getMessage());
@@ -159,14 +181,77 @@ public class FragmentAdminStadistics extends Fragment {
 
                 //refreshContent(jsResponse);
 
-                spinnerTecnicos.setAdapter(arrayAdapter);
+                spinnerEstatus.setAdapter(arrayAdapter);
 
-                //activateProgressBar(false);
-                spinnerTecnicos.setEnabled(true);
+                // Habilitamos el spinner de usuarios.
+                spinnerEstatus.setEnabled(true);
+                // Habilitamos el boton de filtrado.
+                filterButton.setEnabled(true);
+
                 return true;
             }
         });
     }
+
+    /**
+     * Metodo para realizar la request de los usuarios tecnicos.
+     *
+     * @param userToken
+     */
+    private void requestStatusStadistics(String userToken) {
+
+        String url2 = "https://reseed-385107.ew.r.appspot.com/tareas/porTipo-tarea?tarea=analisis";
+
+        JsonArrayGetRequest userListRequest = new JsonArrayGetRequest(userToken,  requestQueue, url);
+        JSONObject jsResponse = new JSONObject();
+
+        //activateProgressBar(true);
+        userListRequest.sendRequest(new VolleyResponseInterface() {
+            @Override
+            public void onError(String message) {
+                Log.e("Error login: ", message);
+                //activateProgressBar(false);
+                Toast toast = Toast.makeText(requireContext(), message, Toast.LENGTH_LONG);
+                toast.show();
+            }
+
+            @Override
+            public boolean onResponse(Object response) {
+                JSONArray jsResponse = (JSONArray) response;
+                Log.i("Respuesta user info para creacion de tarea", jsResponse.toString());
+
+                ArrayAdapter arrayAdapter = null;
+
+                try {
+                    arrayAdapter = new ArrayAdapter(
+                            getContext(),
+                            android.R.layout.simple_spinner_dropdown_item,
+                            arrayUsersName(jsResponse.getJSONArray(0),idsUsers));
+                } catch (JSONException e) {
+                    Log.e("Error peticion users", e.getMessage());
+                }
+
+                //refreshContent(jsResponse);
+
+                spinnerEstatus.setAdapter(arrayAdapter);
+
+                // Habilitamos el spinner de usuarios.
+                spinnerEstatus.setEnabled(true);
+                // Habilitamos el boton de filtrado.
+                filterButton.setEnabled(true);
+
+                return true;
+            }
+        });
+    }
+
+
+    /**
+     * Metodo para preparar los datos de los usuarios para que puedan ser usados en el spinner de usuarios.
+     * @param jsonArrayUsers
+     * @param idsUsers
+     * @return
+     */
     private String[] arrayUsersName(JSONArray jsonArrayUsers, List<Integer> idsUsers) {
 
         this.jsonArrayUsers = jsonArrayUsers;
